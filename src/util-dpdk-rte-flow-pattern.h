1d5857563b10acddf58759476a52725ffed8f352
Adam Kiripolsky <Adam.Kiripolsky@cesnet.cz>
2024-11-07 12:58:24 +0100

    dpdk/rte_flow: add traffic filter
    Added pattern based drop-filter.
    Supported patterns are listed in "util-dpdk-rte-flow-pattern.c" in
    "enum index next_item[]" and their corresponding attributes
    in "enum index item_<pattern>[]".
    
    The syntax of the pattern used in suricata.yaml is taken
    from test-pmd application.
    Other attributes, such as actions, are omitted, and only
    pattern is accepted, e.g., a valid entry in suricata.yaml is:
    "pattern eth / ipv4 src is 192.168.11.12 / end".
    
    The user needs to validate the pattern used for a specific driver first,
    otherwise, Suricata shuts down if the pattern is unsupported.
    Source for supported patterns with different drivers:
    https://doc.dpdk.org/guides/nics/overview.html
    
    More patterns can be added in the future if needed. This new entry
    needs to be specified in the `enum index`. This pattern then needs to be
    present in `enum index next_item[]` and its corresponding attributes in
    `enum index item_<pattern>[]`. Also, new entry needs to be provided to
    `struct token token_list[]` with appropriate attributes.
    
    The feature was tested on NIC drivers mlx5, ice, i40e, and ixgbe.
    
    mlx5 accepts multiple rules with different patterns and various
    corresponding specifications of pattern items. The driver does not have
    any additional restrictions on the pattern.
    
    ice does not support broad patterns; some pattern item has to have
    specification, e.g., pattern "eth / ipv4 / end" raises an error but
    "eth / ipv4 src is x / end" or "eth / ipv4 / tcp src is x" works fine.
    
    i40e does not support different item sets on the same pattern item type,
    e.g., if the first rule has the pattern `eth / ipv4 src is x / end`,
    then if any other rule contains an ipv4 pattern type, it needs to have
    exclusively attribute src.
    
    ixgbe does not support this feature.

---
+++ src/util-dpdk-rte-flow-pattern.h
@@ -0,0 +1,48 @@
+/* Copyright (C) 2024 Open Information Security Foundation
+ *
+ * You can copy, redistribute or modify this Program under the terms of
+ * the GNU General Public License version 2 as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ *  \defgroup dpdk DPDK rte_flow rules util functions
+ *
+ *  @{
+ */
+
+/**
+ * \file
+ *
+ * \author Adam Kiripolsky <adam.kiripolsky@cesnet.cz>
+ *
+ * DPDK rte_flow rules util functions
+ *
+ */
+
+#include "util-dpdk.h"
+
+#ifndef SURICATA_RTE_FLOW_RULES_PATTERN_H
+#define SURICATA_RTE_FLOW_RULES_PATTERN_H
+
+#ifdef HAVE_DPDK
+
+#include <rte_ethdev.h>
+
+int ParsePattern(char *pattern, uint8_t *data, unsigned int size, struct rte_flow_item **items);
+
+#endif /* HAVE_DPDK */
+#endif /* SURICATA_RTE_FLOW_RULES_PATTERN_H */
+/**
+ * @}
+ */
