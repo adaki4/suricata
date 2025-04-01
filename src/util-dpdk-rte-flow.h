4c157d465d8bfe11e3ce55de6aba58f48efa53a7
Adam Kiripolsky <adam.kiripolsky@cesnet.cz>
2025-04-01 19:25:47 +0200

    rules upload on other thread, need to finish bypassManager

--- src/util-dpdk-rte-flow.h
+++ src/util-dpdk-rte-flow.h
@@ -37,6 +37,7 @@
 
 #include "conf.h"
 #include "util-dpdk.h"
+#include "flow-bypass.h"
 
 void RteFlowRuleStorageFree(RteFlowRuleStorage *rte_flow_rule_storage);
 int ConfigLoadRteFlowRules(
@@ -45,7 +46,13 @@ int RteFlowRulesCreate(char *port_name, int port_id, RteFlowRuleStorage *rte_flo
         const char *driver_name);
 uint64_t RteFlowFilteredPacketsQuery(struct rte_flow **rte_flow_rules, uint16_t rule_count,
         char *device_name, int port_id, uint64_t *filtered_packets);
+void RteBypassInit(const char *port_name);
 int RteFlowBypassCallback(Packet *);
+int RteFlowUpdateFlow(Flow *f, Packet *p, void *data);
+int RteFlowCheckBypassedFlowCreate(ThreadVars *th_v, struct timespec *curtime, void *data);
+int RteFlowBypassRuleLoad(void);
+
+
 
 #endif /* HAVE_DPDK */
 #endif /* SURICATA_RTE_FLOW_RULES_H */
