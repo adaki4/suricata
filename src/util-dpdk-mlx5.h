a9c4f61631691ff403510de11264f3f71f5e55ce
Adam Kiripolsky <adam.kiripolsky@cesnet.cz>
2025-10-23 15:31:49 +0200

    change mlx5 max rte_flow rules

--- src/util-dpdk-mlx5.h
+++ src/util-dpdk-mlx5.h
@@ -30,7 +30,7 @@
 
 /* Although 2^16 rules are supported on mlx5 in rte_flow group 0,
    rules in approximately top 10% of the capacity do not support counters */
-#define MLX5_RTE_FLOW_RULES_CAPACITY 57000
+#define MLX5_RTE_FLOW_RULES_CAPACITY 52000
 
 int mlx5DeviceSetRSS(int port_id, uint16_t nb_rx_queues, char *port_name);
 int mlx5DeviceCheckDropFilterLimits(uint32_t rte_flow_rule_count, char **err_msg);
